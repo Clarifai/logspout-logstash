6877e64a5298e80c83d6d1736209db97af6ba943
Adam Berenzweig <madadam@clarifai.com>
2015-12-03 12:34:38 -0500

    Parse structured data from message json; transport option; logz.io token

--- logstash.go
+++ logstash.go
@@ -3,8 +3,10 @@ package logstash
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/gliderlabs/logspout/router"
 )
@@ -13,15 +15,24 @@ func init() {
 	router.AdapterFactories.Register(NewLogstashAdapter, "logstash")
 }
 
-// LogstashAdapter is an adapter that streams UDP JSON to Logstash.
+// LogstashAdapter is an adapter that streams TCP JSON to Logstash.
 type LogstashAdapter struct {
 	conn  net.Conn
 	route *router.Route
 }
 
-// NewLogstashAdapter creates a LogstashAdapter with UDP as the default transport.
+func getopt(name, dfault string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		value = dfault
+	}
+	return value
+}
+
+// NewLogstashAdapter creates a LogstashAdapter with TCP as the default transport.
 func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
-	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport("udp"))
+	t := getopt("LOGSTASH_TRANSPORT", "tcp")
+	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport(t))
 	if !found {
 		return nil, errors.New("unable to find adapter: " + route.Adapter)
 	}
@@ -40,12 +51,24 @@ func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
 // Stream implements the router.LogAdapter interface.
 func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 	for m := range logstream {
+		var data interface{}
+		if m.Data != "" {
+			err := json.Unmarshal([]byte(m.Data), &data)
+			if err != nil {
+				// FIXME: how to safely log errors not messing up downstream schema?
+				data = fmt.Sprintf("ERROR: Failed to parse json: %s", err)
+			}
+		}
+
+		token := getopt("LOGSTASH_LOGZIO_TOKEN", "")
+
 		msg := LogstashMessage{
-			Message:  m.Data,
+			Data:     data,
 			Name:     m.Container.Name,
 			ID:       m.Container.ID,
 			Image:    m.Container.Config.Image,
 			Hostname: m.Container.Config.Hostname,
+			Token:    token,
 		}
 		js, err := json.Marshal(msg)
 		if err != nil {
@@ -62,9 +85,10 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 
 // LogstashMessage is a simple JSON input to Logstash.
 type LogstashMessage struct {
-	Message  string `json:"message"`
-	Name     string `json:"docker.name"`
-	ID       string `json:"docker.id"`
-	Image    string `json:"docker.image"`
-	Hostname string `json:"docker.hostname"`
+	Name     string      `json:"docker.name"`
+	ID       string      `json:"docker.id"`
+	Image    string      `json:"docker.image"`
+	Hostname string      `json:"docker.hostname"`
+	Data     interface{} `json:"data,omitempty"`
+	Token    string      `json:"token,omitempty"` // logz.io token
 }
